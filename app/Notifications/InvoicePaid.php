6188920b5aa71132950a17618edb699010ba3066
imranbloach <dev.imran.pk@gmail.com>
2023-09-06 08:57:41 -0700

    first commit

---
+++ app/Notifications/InvoicePaid.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoicePaid extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $invoice;
+
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'invoice_id' => $this->invoice->id,
+            'message' => 'Invoice ' . $this->invoice->invoice_number . ' has been paid.',
+        ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'invoice_id' => $this->invoice->id,
+            'message' => 'Invoice ' . $this->invoice->invoice_number . ' has been paid.',
+        ];
+    }
+}
---
+++ database/seeders/InvoicesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InvoicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
